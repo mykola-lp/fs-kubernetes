173451522878c85785433cb47f23d8d84204e228
mykola-lp <mykola.sukhorab@gmail.com>
2026-09-20 12:49:02 +0300

    upd pingpong to persist counter to shared volume

--- pingpong/index.js
+++ pingpong/index.js
@@ -1,13 +1,22 @@
 const http = require('http');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
+const filePath = '/usr/src/app/files/counter.txt';
+
 let counter = 0;
+
+if (fs.existsSync(filePath)) {
+  counter = parseInt(fs.readFileSync(filePath, 'utf-8')) || 0;
+}
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET' && req.url === '/pingpong') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end(`pong ${counter}`);
     counter++;
+
+    fs.writeFileSync(filePath, counter.toString());
     return;
   }
 
